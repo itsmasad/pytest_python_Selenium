8befdb0e25e246dba5125456351ae00ffc52c2d4
itsmasad <muhammadasadofficial@gmail.com>
2024-02-16 19:04:07 +0500

    mouse and keyboard actions

---
+++ Demo Project/Mouse_Keyboard_Actions.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+website = ""
+driver.get(webdriver)
+driver.maximize_window()
+
+
+time.sleep(3)
+driver.quit()

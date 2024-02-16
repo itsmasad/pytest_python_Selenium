9b422d2512939c36ae005c7be6d486fd6f28d3f2
itsmasad <muhammadasadofficial@gmail.com>
2024-02-16 19:24:56 +0500

    updated

--- Demo Project/Mouse_Keyboard_Actions.py
+++ Demo Project/Mouse_Keyboard_Actions.py
@@ -4,12 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 
 driver = webdriver.Chrome()
-website = ""
-driver.get(webdriver)
+website = "https://magento.softwaretestingboard.com/"
+driver.get(website)
 driver.maximize_window()
 
+# ActionChains - mouse movements, mouse button actions, keypress, and context menu interactions
+women = driver.find_element(By.XPATH, "//a[@id='ui-id-4']")
+women_top = driver.find_element(By.XPATH, "//a[@id='ui-id-9']")
+women_top_jacket = driver.find_element(By.XPATH, "//a[@id='ui-id-11']")
+
+
+action = ActionChains(driver)
+action.move_to_element(women).perform()
+action.move_to_element(women_top).perform()
+action.click(women_top_jacket)
 
 time.sleep(3)
 driver.quit()

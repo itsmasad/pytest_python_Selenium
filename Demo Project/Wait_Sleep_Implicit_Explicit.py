bbb4a03f4ec23452f74aba53912eb7d05bfe8205
itsmasad <muhammadasadofficial@gmail.com>
2024-02-16 19:00:56 +0500

    Wait complete

--- Demo Project/Wait_Sleep_Implicit_Explicit.py
+++ Demo Project/Wait_Sleep_Implicit_Explicit.py
@@ -6,9 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-website = ""
+website = "https://demo.automationtesting.in/Index.html"
 driver.get(website)
-driver.maximize_window
+driver.maximize_window()
 
 # Wait Slee (this is basic python wait)
 time.sleep(5)
@@ -21,6 +21,15 @@ driver.implicitly_wait(3)
 
 # Wait Explicit Wait - hey allow your code to halt program execution, or freeze the thread, until the condition you pass it resolves.
 # The condition is called with a certain frequency until the timeout of the wait is elapsed.
+# Mostly use to wait for a particular element to apear
+
+def Wait(by, selector):
+    return WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((by, selector))
+    )
+
+Wait(By.ID, "email")
+driver.find_element(By.ID, "email").send_keys("email@gmail.com")
 
 time.sleep(5)
 driver.quit()

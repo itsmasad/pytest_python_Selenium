476c9f9fc0b7ff30bd14c601b67e4e52e838199b
itsmasad <muhammadasadofficial@gmail.com>
2024-02-20 21:14:24 +0500

    pytest updated

--- pytest_testcases/test_fixtureScope.py
+++ pytest_testcases/test_fixtureScope.py
@@ -7,16 +7,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
+# Fixture will run everytime before any method
+# Module will run one time before all methods
 
 @pytest.fixture(scope="function")
 def launchbrowser():
     # Make a keyword global so it can be used everywhare
     global driver
     driver = webdriver.Chrome()
+    yield # After method
+    driver.quit()
 
 
 
 # pass the fixture method name in function to pass the data
+
 def test_printUrl(launchbrowser):
     driver.get("https://docs.pytest.org/en/6.2.x/fixture.html")
+
+
+def test_printUrl(launchbrowser):
     print(driver.current_url)

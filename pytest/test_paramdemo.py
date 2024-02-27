89a3d976e2a8195b191d74cbd287f05add1704a5
itsmasad <muhammadasadofficial@gmail.com>
2024-02-27 15:28:41 +0500

    parametrize

--- pytest/test_paramdemo.py
+++ pytest/test_paramdemo.py
@@ -1,16 +1,5 @@
 import pytest
 
-@pytest.fixture(params=["a","b"])
-def demo_fixture(request):
-    print(request.param)
-
-def testLogin(demo_fixture):
-    print("Login Successful")
-
-
-def testLogoff():
-    print("Logout Successful")
-
-
-def testCalculation():
-    assert 2 + 2 == 4
+@pytest.mark.parametrize("username,password",[("asad","1234"),("sajid","5678"),("naveed","0000")])
+def test_sampleone(username,password):
+    print(username+"-"+password)

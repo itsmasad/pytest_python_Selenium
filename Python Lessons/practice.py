7c5e447740a9712f77bd7bd5323f71206faa2333
itsmasad <muhammadasadofficial@gmail.com>
2024-03-04 14:32:17 +0500

    updated practice

---
+++ Python Lessons/practice.py
@@ -0,0 +1,32 @@
+# List
+numbers = [1,2,3,4,5]
+print(numbers[2])
+print(len(numbers))
+print(type(numbers))
+numbers.append('Mangos')
+numbers.remove(numbers[1])
+print(numbers)
+
+
+# Variables
+x = 1
+y = "two"
+z = 2.9
+a = True
+print(x,y,z,a)
+
+print(f"These are the variables {x,y,z,a}")
+
+# tuple & sets
+fruits = ('mango','banana','orange')
+fruits1 = {'mango','banana','orange'}
+print(type(fruits),type(fruits1))
+
+# Loops
+# for i in range(6):
+#     print('* '*i)
+
+count = 5
+for i in range(count):
+    limit = count - i
+    print('* ' * limit)

816d11004c9a73a142835e6f53c926e88b18b60d
itsmasad <muhammadasadofficial@gmail.com>
2024-03-04 15:55:45 +0500

    Updated pythone constructor

---
+++ Python Lessons/Constructors.py
@@ -0,0 +1,39 @@
+# Constructors
+
+# It is a special method within a class that gets automatically called when an object or instance of the class is created.
+# It is used to initialize the attributes of the object.
+# It is writen with __init__
+# The 'self' keyword is used within the methods of a class to refer to the instance of 
+# the class itself. It is a convention and not a reserved keyword, 
+# but it is widely adopted and recommended for clarity.
+
+
+# class faculty:
+#     def putdata(self):
+#         self.id=int((input("Enter Faculty ID")))
+#         self.name=input("Enter Name")
+#         self.salary=float(input("Enter Salary"))
+#     def display(self):
+#         print(f'Faculty ID : {self.id}')
+#         print(f'Faculty Name :{self.name}')
+#         print(f'Faculty Salary : {self.salary}')
+
+# a = faculty()
+# a.putdata()
+# a.display()
+
+
+
+class faculty:
+    def __init__(self):
+        self.id=int((input("Enter Faculty ID")))
+        self.name=input("Enter Name")
+        self.salary=float(input("Enter Salary"))
+    def display(self):
+        print(f'Faculty ID : {self.id}')
+        print(f'Faculty Name :{self.name}')
+        print(f'Faculty Salary : {self.salary}')
+
+a = faculty()
+# Here we don't need to call init, it will run automatically when an object is called or created
+a.display()

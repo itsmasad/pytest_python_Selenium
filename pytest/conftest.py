825c4017d521dc6bb1d6b78a7dc0d556c8fb7d0d
itsmasad <muhammadasadofficial@gmail.com>
2024-03-21 03:06:46 +0500

    updated code

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -1,3 +1,10 @@
+# In Pytest, the conftest.py file is used to define fixtures, hooks,
+# and other configurations that can be shared across multiple test files
+# within a directory or package. It serves as a central location for storing common
+# fixtures and configuration settings used by tests.
+
+
+
 import pytest
 
 @pytest.fixture(scope="session", autouse=True)

020f5c64e093d90f5d0c2d4f97655d9dab422a2d
itsmasad <muhammadasadofficial@gmail.com>
2024-03-21 07:53:40 +0500

    updated code

--- pytest_selenium/conftest.py
+++ pytest_selenium/conftest.py
@@ -1,9 +1,17 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 
-def wait():
-    return time.sleep(5)
+def wait(by,locator):
+    return WebDriverWait(driver=5).until(
+        EC.presence_of_element_located((by, locator))
+    )
+
 
 @pytest.fixture(scope="class",autouse=True)
 def browser():
@@ -11,5 +19,6 @@ def browser():
  
     yield driver
     driver.close()
+    
 
 
--- pytest_selenium/test_login.py
+++ pytest_selenium/test_login.py
@@ -1,6 +1,14 @@
+import time
 from conftest import wait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
 
 class TestLogin:
     def test_navigate_to_google(self, browser):
-        browser.get("https://www.google.com")
-        wait()
+        driver.get("https://www.google.com")
+        # wait(By.CLASS_NAME,"gLFyf")
+        search = driver.find_element(By.CLASS_NAME,"gLFyf")
+        search.send_keys("Asad")
+        time.sleep(5)

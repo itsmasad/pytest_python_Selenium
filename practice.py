0e2d4fe5ea045818ca9c57664437e4c0bc6ddc30
itsmasad <muhammadasadofficial@gmail.com>
2024-03-29 03:21:43 +0500

    anagram string function

--- practice.py
+++ practice.py
@@ -1,4 +1,20 @@
+# Checking if the number is divisible by 10
+# for i in range(1, 100):
+#     if i % 10 == 0:
+#         print(i)
 
-for i in range(1, 100):
-    if i % 2 == 0:
-        print(i)
+
+# Checking Anagram Function
+# def is_anagram(str1, str2):
+#     str1 = str1.replace(" ","").lower()
+#     str2 = str2.replace(" ","").lower()
+
+#     return sorted(str1) == sorted(str2)
+
+# word1 = "silent"
+# word2 = "listen"
+
+# if is_anagram(word1,word2):
+#     print("It is an anagram")
+# else:
+#     print("not an anagram")

9b38be071e7bbceb5712eae5fd047d93eb4d3e99
itsmasad <muhammadasadofficial@gmail.com>
2024-03-31 00:23:46 +0500

    updated code

--- Demo Project/Screenshot.py
+++ Demo Project/Screenshot.py
@@ -5,7 +5,10 @@ driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 driver.maximize_window()
 
+# Method 1
 driver.get_screenshot_as_file("C:\\Users\\Asad\\Desktop\\Selenium Projects\\Demo Project\\screenshots\\screenshot.jpeg")
+# Method 2
+driver.save_screenshot("C:\\Users\\Asad\\Desktop\\Selenium Projects\\Demo Project\\screenshots\\screenshot2.jpeg")
 
 time.sleep(5)
 driver.quit()

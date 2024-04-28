a5aa9e926421d8fc12a32a6d78570fc79eb0fc90
itsmasad <muhammadasadofficial@gmail.com>
2024-04-29 02:18:51 +0500

    updated comments

--- pytest/self.py
+++ pytest/self.py
@@ -9,12 +9,20 @@
 # This parameter refers to the instance of the class itself and allows the method to access and modify attributes of the instance.
 
 
+# Define a class named MyClass
 class MyClass:
+    # Define the constructor method (__init__) which initializes the instance with a value x
     def __init__(self, x):
+        # Assign the value of x to the instance attribute self.x
         self.x = x
 
+    # Define a method named print_value which prints the value of self.x
     def print_value(self):
         print(self.x)
 
+# Create an instance of MyClass with the value 5 and assign it to the variable obj
 obj = MyClass(5)
+
+# Call the print_value method of the obj instance which prints the value of self.x
 obj.print_value()  # Output: 5
+

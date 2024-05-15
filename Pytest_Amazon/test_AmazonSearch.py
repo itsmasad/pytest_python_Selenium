29d42a1796572efe2f7e34792ea8ba381b981ccc
itsmasad <muhammadasadofficial@gmail.com>
2024-05-15 19:51:20 +0500

    Updated code with some comments

--- Pytest_Amazon/test_AmazonSearch.py
+++ Pytest_Amazon/test_AmazonSearch.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_Search(setUp,self):
+def test_Search(setUp):
     driver = setUp
     driver.get("https://www.google.com/")
     searchBox = driver.find_element(By.CLASS_NAME, "gLFyf")

0dbce651546381b98dcb9075c863b8e9c40bce51
itsmasad <muhammadasadofficial@gmail.com>
2024-05-15 19:39:11 +0500

    updated crossbrowser testing feature

--- Pytest_Amazon/conftest.py
+++ Pytest_Amazon/conftest.py
@@ -5,9 +5,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(scope="class")
-def setUp():
-    driver = webdriver.Chrome()
+@pytest.fixture(scope="class",params=["chrome", "firefox", "edge"])
+def setUp(request):
+
+    # To run this code from command line browser specific you can use this command
+    pytest -k 
+
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    elif request.param == "edge":
+        driver = webdriver.Edge()
+
+    # driver = webdriver.Chrome()
+
+
     yield driver
     time.sleep(3)
-    driver.close()
+    driver.quit()
--- Pytest_Amazon/test_AmazonSearch.py
+++ Pytest_Amazon/test_AmazonSearch.py
@@ -5,13 +5,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_Search(setUp):
+def test_Search(setUp,self):
     driver = setUp
     driver.get("https://www.google.com/")
     searchBox = driver.find_element(By.CLASS_NAME, "gLFyf")
     searchBox.send_keys("amazone")
     time.sleep(4)
-    # results = driver.find_element(By.XPATH,"(//*[@class='lnnVSe'])[1]")
     list_1 = []
     for i in range(1,8):
         xpath = "(//*[@class='lnnVSe'])[{i}]".format(i=i)  # Corrected string formatting

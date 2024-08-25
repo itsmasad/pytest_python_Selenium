a1313e2dd05ba1e894e277a105b8903d4e8f9477
itsmasad <Muhammadasadofficial@gmail.com>
2024-08-25 14:17:59 +0500

    updated

--- Interview_code_questions.py
+++ Interview_code_questions.py
@@ -324,22 +324,22 @@
 
 # To get the 5 consecutive number of any number, i.e. 850
 
-def solution(n):
-    verify = False  # Flag to check if a sequence is found
-    for i in range(1, n+1):  # Iterate over possible starting points of sequences
-        count = 0  # Reset the sum of consecutive numbers for each starting point
-        if not verify:  # If a sequence hasn't been found yet
-            for b in range(i, i+5):  # Iterate over 5 consecutive numbers starting from i
-                count += b  # Add the current number to the sum
-            if count == n:  # If the sum matches the target number
-                print(n)  # Print the target number
-                print(f'{i},{i+1},{i+2},{i+3},{i+4}')  # Print the consecutive sequence
-                verify = True  # Set the flag to indicate a sequence has been found
-                break  # Exit the inner loop since the sequence is found
-    if not verify:  # If no sequence is found after checking all possible starting points
-        print("No consecutive sequence found for", n)  # Print a message indicating no sequence was found
+# def solution(n):
+#     verify = False  # Flag to check if a sequence is found
+#     for i in range(1, n+1):  # Iterate over possible starting points of sequences
+#         count = 0  # Reset the sum of consecutive numbers for each starting point
+#         if not verify:  # If a sequence hasn't been found yet
+#             for b in range(i, i+5):  # Iterate over 5 consecutive numbers starting from i
+#                 count += b  # Add the current number to the sum
+#             if count == n:  # If the sum matches the target number
+#                 print(n)  # Print the target number
+#                 print(f'{i},{i+1},{i+2},{i+3},{i+4}')  # Print the consecutive sequence
+#                 verify = True  # Set the flag to indicate a sequence has been found
+#                 break  # Exit the inner loop since the sequence is found
+#     if not verify:  # If no sequence is found after checking all possible starting points
+#         print("No consecutive sequence found for", n)  # Print a message indicating no sequence was found
 
-solution(851)
+# solution(851)
 
 # Another way of doing this
 

6d4fbf6dea07694d6f90f8af3e552aee9ffecbdc
itsmasad <muhammadasadofficial@gmail.com>
2024-10-24 05:45:48 +0500

    Anagram

--- PracticeForInterview.py
+++ PracticeForInterview.py
@@ -5,3 +5,14 @@
 
 # import random
 # print(random.uniform(0.1,99))
+
+# Checking anagram
+# def solution(str1,str2):
+#     new1 = sorted(str1.lower())
+#     new2 = sorted(str2.lower())
+#     if new1==new2:
+#         print('Anagram')
+#     else:
+#         print('Not anagram')
+
+# solution('Silent','Listen')
